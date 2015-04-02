35c32caaff18f7f3982347add7ff1c4c210916b8
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-04-02 14:55:46 -0400

    Fix timeouts?

--- test/marina_tests.erl
+++ test/marina_tests.erl
@@ -1,30 +1,21 @@
 -module(marina_tests).
 -include("test.hrl").
 
-%% runner
-marina_test() ->
-    % setup
+%% tests
+marina_test_() ->
     setup_schema(),
     set_keyspace(),
 
-    % tests
-    test_query(),
-    test_reusable_query().
+    {inparallel, [
+        test_async_query(),
+        test_query(),
+        test_reusable_query()
+    ]}.
 
-%% tests
-test_query() ->
-    Response = query(<<"INSERT INTO users (key, column1, column2, value) values (99492dfe-d94a-11e4-af39-58f44110757d, 'test', 'test2', intAsBlob(0))">>),
-    ?assertEqual({ok, undefined}, Response).
-
-test_reusable_query() ->
-    Response = reusable_query(<<"SELECT * FROM user LIMIT 1;">>, []),
-    ?assertEqual({error, {8704, <<"unconfigured columnfamily user">>}}, Response),
-
-    Response2 = reusable_query(<<"SELECT * FROM users LIMIT 1;">>, []),
-    Response2 = reusable_query(<<"SELECT * FROM users LIMIT 1;">>, []),
-    Response2 = reusable_query(<<"SELECT * FROM users WHERE key = ?;">>, [<<153,73,45,254,217,74,17,228,175,57,88,244,65,16,117,125>>], 1),
-
-    ?assertEqual({ok,
+test_async_query() ->
+    {ok, Ref} = async_query(<<"SELECT * FROM users LIMIT 1;">>),
+    Response = receive_response(Ref),
+    ?_assertEqual({ok,
         {result,
             {result_metadata, 4, [
                 {column_spec,<<"test">>,<<"users">>,<<"key">>,uid},
@@ -34,7 +25,38 @@ test_reusable_query() ->
             ]}, 1, [
                 [<<153,73,45,254,217,74,17,228,175,57,88,244,65,16,117,125>>, <<"test">>, <<"test2">>, <<0,0,0,0>>]
         ]}
-    }, Response2).
+    }, Response).
+
+test_query() ->
+    Response = query(<<"SELECT * FROM users LIMIT 1;">>),
+    ?_assertEqual({ok,
+        {result,
+            {result_metadata, 4, [
+                {column_spec,<<"test">>,<<"users">>,<<"key">>,uid},
+                {column_spec,<<"test">>,<<"users">>,<<"column1">>,varchar},
+                {column_spec,<<"test">>,<<"users">>,<<"column2">>,varchar},
+                {column_spec,<<"test">>,<<"users">>,<<"value">>,blob}
+            ]}, 1, [
+                [<<153,73,45,254,217,74,17,228,175,57,88,244,65,16,117,125>>, <<"test">>, <<"test2">>, <<0,0,0,0>>]
+        ]}
+    }, Response).
+
+test_reusable_query() ->
+    Response = reusable_query(<<"SELECT * FROM users LIMIT 1;">>, []),
+    Response = reusable_query(<<"SELECT * FROM users LIMIT 1;">>, []),
+    Response = reusable_query(<<"SELECT * FROM users WHERE key = ?;">>, [<<153,73,45,254,217,74,17,228,175,57,88,244,65,16,117,125>>], 1),
+
+    ?_assertEqual({ok,
+        {result,
+            {result_metadata, 4, [
+                {column_spec,<<"test">>,<<"users">>,<<"key">>,uid},
+                {column_spec,<<"test">>,<<"users">>,<<"column1">>,varchar},
+                {column_spec,<<"test">>,<<"users">>,<<"column2">>,varchar},
+                {column_spec,<<"test">>,<<"users">>,<<"value">>,blob}
+            ]}, 1, [
+                [<<153,73,45,254,217,74,17,228,175,57,88,244,65,16,117,125>>, <<"test">>, <<"test2">>, <<0,0,0,0>>]
+        ]}
+    }, Response).
 
 %% setup
 setup_schema() ->
@@ -42,6 +64,7 @@ setup_schema() ->
     query(<<"DROP KEYSPACE test;">>),
     query(<<"CREATE KEYSPACE test WITH REPLICATION = {'class':'SimpleStrategy', 'replication_factor':1};">>),
     query(<<"CREATE TABLE test.users (key uuid, column1 text, column2 text, value blob, PRIMARY KEY (key, column1, column2));">>),
+    query(<<"INSERT INTO users (key, column1, column2, value) values (99492dfe-d94a-11e4-af39-58f44110757d, 'test', 'test2', intAsBlob(0))">>),
     application:stop(marina).
 
 set_keyspace() ->
@@ -50,8 +73,19 @@ set_keyspace() ->
     application:start(marina).
 
 %% helpers
+async_query(Query) ->
+    marina:async_query(Query, ?CONSISTENCY_ONE, ?DEFAULT_FLAGS, self()).
+
 query(Query) ->
     marina:query(Query, ?CONSISTENCY_ONE, ?DEFAULT_FLAGS, ?TEST_TIMEOUT).
+
+receive_response(Ref) ->
+    receive
+        {?APP, Ref, Reply} ->
+            marina:response(Reply)
+    after ?TEST_TIMEOUT ->
+        {error, timeout}
+    end.
 
 reusable_query(Query, Values) ->
     reusable_query(Query, Values, ?TEST_TIMEOUT).

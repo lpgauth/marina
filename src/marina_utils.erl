8efaf74e12274b01966a1ee04bf38a8f728b4229
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-04-06 20:58:35 -0400

    Add missing specs

--- src/marina_utils.erl
+++ src/marina_utils.erl
@@ -2,7 +2,7 @@
 -include("marina.hrl").
 
 -export([
-    childs_specs/0,
+    child_specs/0,
     child_name/1,
     info_msg/2,
     pack/1,
@@ -13,15 +13,23 @@
 ]).
 
 %% public
-childs_specs() ->
+-spec child_specs() -> [supervisor:child_spec()].
+
+child_specs() ->
     PoolSize = application:get_env(?APP, pool_size, ?DEFAULT_POOL_SIZE),
     [child_spec(N) || N <- lists:seq(1, PoolSize)].
+
+-spec child_name(integer()) -> atom().
 
 child_name(N) ->
     list_to_atom(?SERVER_BASE_NAME ++ integer_to_list(N)).
 
+-spec info_msg(string(), [term()]) -> ok.
+
 info_msg(Format, Data) ->
     error_logger:info_msg(Format, Data).
+
+-spec pack(binary() | iolist()) -> {ok, binary()} | {error, term()}.
 
 pack(Iolist) when is_list(Iolist) ->
     pack(iolist_to_binary(Iolist));
@@ -33,6 +41,8 @@ pack(Binary) ->
             Error
     end.
 
+-spec timeout(erlang:timestamp(), non_neg_integer()) -> non_neg_integer().
+
 timeout(Timestamp, Timeout) ->
     Diff = timer:now_diff(os:timestamp(), Timestamp) div 1000,
     case Timeout - Diff of
@@ -40,8 +50,12 @@ timeout(Timestamp, Timeout) ->
         X -> X
     end.
 
+-spec unpack(binary()) -> {ok, binary()} | {error, term()}.
+
 unpack(<<Size:32/unsigned-integer, Binary/binary>>) ->
     lz4:uncompress(Binary, Size).
+
+-spec warning_msg(string(), [term()]) -> ok.
 
 warning_msg(Format, Data) ->
     error_logger:warning_msg(Format, Data).

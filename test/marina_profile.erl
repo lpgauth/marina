049ab094e11c3481b82fb2129611da63d061eeec
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-02-07 22:40:02 -0500

    Improve profile target

--- test/marina_profile.erl
+++ test/marina_profile.erl
@@ -17,20 +17,25 @@ fprofx() ->
         Filename <- Filenames],
     lists:foreach(fun code:load_abs/1, Rootnames),
 
+    marina_app:start(),
+    setup(),
+    marina_app:stop(),
+
     fprofx:start(),
     {ok, Tracer} = fprofx:profile(start),
     fprofx:trace([start, {procs, new}, {tracer, Tracer}]),
 
     Self = self(),
     Query = <<"SELECT * FROM rtb.users WHERE key = ?;">>,
-    Uid = <<115, 123, 69, 190, 177, 106, 17, 229, 160, 86, 27, 159, 238, 8, 37,
-        193>>,
+    Uid = <<153, 73, 45, 254, 217, 74, 17, 228, 175, 57, 88,
+        244, 65, 16, 117, 125>>,
+    Opts = [{skip_metadata, true}],
 
     marina_app:start(),
 
     [spawn(fun () ->
-        [marina:reusable_query(Query, 10, [Uid], 1000) ||
-            _ <- lists:seq(1, ?N)],
+        [marina:reusable_query(Query, [Uid], ?CONSISTENCY_LOCAL_ONE,
+            Opts, ?TIMEOUT) || _ <- lists:seq(1, ?N)],
         Self ! exit
     end) || _ <- lists:seq(1, ?P)],
     wait(),
@@ -43,6 +48,18 @@ fprofx() ->
     ok.
 
 %% private
+setup() ->
+    [marina:query(Query, ?CONSISTENCY_LOCAL_ONE, [], ?TIMEOUT) || Query <- [
+        <<"DROP KEYSPACE test;">>,
+        <<"CREATE KEYSPACE test WITH REPLICATION =
+            {'class':'SimpleStrategy', 'replication_factor':1};">>,
+        <<"CREATE TABLE test.users (key uuid, column1 text,
+            column2 text, value blob, PRIMARY KEY (key, column1, column2));">>,
+        <<"INSERT INTO test.users (key, column1, column2, value)
+            values (99492dfe-d94a-11e4-af39-58f44110757d, 'test', 'test2',
+            intAsBlob(0))">>
+    ]].
+
 wait() ->
     wait(?P).
 

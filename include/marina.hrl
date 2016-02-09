2df19891453ff7783919ab6b5793d5269fb25450
Louis-Philippe Gauthier <lpgauth@gmail.com>
2016-02-09 10:57:41 -0500

    Use round_robin pool strategy by default

--- include/marina.hrl
+++ include/marina.hrl
@@ -9,7 +9,7 @@
 -define(DEFAULT_FLAGS, 0).
 -define(DEFAULT_IP, "127.0.0.1").
 -define(DEFAULT_POOL_SIZE, 16).
--define(DEFAULT_POOL_STRATEGY, random).
+-define(DEFAULT_POOL_STRATEGY, round_robin).
 -define(DEFAULT_PORT, 9042).
 -define(DEFAULT_RECONNECT, true).
 -define(DEFAULT_RECONNECT_MAX, timer:minutes(2)).

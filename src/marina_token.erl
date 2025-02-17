adfa36a9b13d3cd582a962b6ac024076c4eff848
Rémi Attab <remi@marketops.com>
2025-02-17 16:15:45 -0500

    Add integer routing key support

--- src/marina_token.erl
+++ src/marina_token.erl
@@ -6,10 +6,13 @@
 ]).
 
 %% public
--spec m3p(binary()) ->
+-spec m3p(integer() | binary()) ->
     integer().
 
-m3p(Key) ->
+m3p(Key) when is_integer(Key) ->
+    Key;
+
+m3p(Key) when is_binary(Key) ->
     <<Hash:64/signed-little-integer, _/binary>> =
         murmur:murmur3_cassandra_x64_128(Key),
     Hash.
